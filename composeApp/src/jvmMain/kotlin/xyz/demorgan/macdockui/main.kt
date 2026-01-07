fd9402f614d7257931196b02620042d3daf950d2
Kir Demorgan <drankov.alex@yandex,ru>
2026-01-07 22:20:02 +0500

    Update window title and icon

--- composeApp/src/jvmMain/kotlin/xyz/demorgan/macdockui/main.kt
+++ composeApp/src/jvmMain/kotlin/xyz/demorgan/macdockui/main.kt
@@ -3,6 +3,9 @@ package xyz.demorgan.macdockui
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import io.github.oshai.kotlinlogging.KotlinLogging
+import macdockui.composeapp.generated.resources.Res
+import macdockui.composeapp.generated.resources.icon
+import org.jetbrains.compose.resources.painterResource
 import javax.swing.SwingUtilities
 import javax.swing.UIManager
 
@@ -13,11 +16,15 @@ fun main() = application {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
         } catch (e: Exception) {
-            logger.error(e) { "Error setting look and feel ${e.message}" }
+            logger.error(e) { "Error setting LookAndFeel: ${e.message}" }
         }
     }
 
-    Window(onCloseRequest = ::exitApplication, title = "Settings") {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "MacDock UI",
+        icon = painterResource(Res.drawable.icon)
+    ) {
         SettingsScreen()
     }
 }
